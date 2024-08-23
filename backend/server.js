0631a555abda89b485e6e2b1c0400fb58d0cdf88
Aravind Shajan <aravindshajan6@gmail.com>
2024-08-23 14:48:42 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use("/api/products", productsRoutes);
 
 // Serve the frontend's index.html for other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3010, () => {
